a8b3d22b9a545334dc4e5e72369c784423e3cf0c
Augustin Ribreau <augustin.ribreau@gmail.com>
2018-12-09 12:55:38 +0100

    init

---
+++ index.php
@@ -0,0 +1,232 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Augustin Ribreau</title>
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+    <link rel="shortcut icon" type="image/png" href="img/favicon.png"/>
+    <link rel="stylesheet" href="styles.css">
+  </head>
+  <body>
+    <header>
+      <img src="img/logo_green.png" alt="LogoAR">
+      <div>
+        <a href="#about">ABOUT</a>
+        <a href="#skills">SKILLS</a>
+        <a href="#contact">CONTACT</a>
+        <a href="#blog">BLOG</a>
+      </div>
+    </header>
+    <div class="First_zone">
+      <div></div>
+      <div></div>
+    </div>
+    <div id="contenair">
+      <img src="img/me.jpg" alt="Augustin Ribreau">
+      <div>
+        <p>I'm <b>Augustin Ribreau</b></p>
+        <p>Developper</p>
+        <hr>
+        <div>
+          <table>
+            <tr>
+              <td>AGE :</td>
+              <td>18</td>
+            </tr>
+            <tr>
+              <td>E-MAIL :</td>
+              <td><a href="mailto:augustin.ribreau@gmail.com">augustin.ribreau@gmail.com</a></td>
+            </tr>
+            <tr>
+              <td>PHONE :</td>
+              <td>Please send an E-mail to get my number.</td>
+            </tr>
+            <tr>
+              <td>ADDRESS :</td>
+              <td>NANTES</td>
+            </tr>
+          </table>
+        </div>
+      </div>
+    </div>
+    <div class="second_zone">
+      <div id="about">
+          Cheers !
+          <br />
+          I'm Augustin Ribreau. I am currently in my first year at EPSI, an engineering school in NANTES.
+          <br />
+          I learn different <p>programming languages</p> as well as notions of <p>system and network administration</p>
+          My skills acquired to date are located >
+          <a href="#skills">here</a>.
+      </div>
+      <div id="skills">
+        <p class="text">Professional Skills</p>
+        <div class="section_box">
+          <div class="column">
+            <div class="row">
+              <p>HTML</p>
+              <div class="bar-line" id="HTML">
+                <div></div>
+              </div>
+            </div>
+            <div class="row">
+              <p>CSS</p>
+              <div class="bar-line" id="CSS">
+                <div></div>
+              </div>
+            </div>
+            <div class="row">
+              <p>C</p>
+              <div class="bar-line" id="C">
+                <div></div>
+              </div>
+            </div>
+            <div class="row">
+              <p>WINDOWS</p>
+              <div class="bar-line" id="WINDOWS">
+                <div></div>
+              </div>
+            </div>
+          </div>
+          <div class="column">
+            <div class="row">
+              <p>PHP</p>
+              <div class="bar-line" id="PHP">
+                <div></div>
+              </div>
+            </div>
+            <div class="row">
+              <p>JAVASCRIPT</p>
+              <div class="bar-line" id="JS">
+                <div></div>
+              </div>
+            </div>
+            <div class="row">
+              <p>SQL</p>
+              <div class="bar-line" id="SQL">
+                <div></div>
+              </div>
+            </div>
+            <div class="row">
+              <p>LINUX</p>
+              <div class="bar-line" id="LINUX">
+                <div></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div id="interest">
+        <p class="text">My Interests</p>
+        <div class="interest-box">
+          <a>Work is essential to man's <p>happiness</p> it elevates him, it consoles him;<br/> and no matter what the nature of the work, as long as it benefits someone :<br /> doing what you can is doing what you should.</a>
+          <div class="interest-row">
+            <div><a><span>DIY</span></a></div>
+            <div><a><span>PC</span></a></div>
+            <div><a><span>Scouting</span></a></div>
+            <div><a><span>Nature</span></a></div>
+            <div><a><span>Skate</span></a></div>
+          </div>
+        </div>
+      </div>
+      <div id="quotes">
+        <p class="text">Favorite quotes</p>
+        <div class="quotes-box">
+          <a class="item-1">"A life without principle is a boat <p>without</p> a rudder."
+            <p class="autor">Gandhi</p>
+          </a>
+          <a class="item-2">"That's not <p>untrue</p>."
+            <p class="autor">Kaamelott</p>
+          </a>
+          <a class="item-3">"But you know, you can find <p>happiness</p> even in the darkest places.<br /> Just remember to turn on the <p>light</p>."
+            <p class="autor">Hary Potter</p>
+          </a>
+        </div>
+      </div>
+      <div id="insta">
+        <p class="text">Instagram feed</p>
+        <div class="insta-box">
+          <div class="insta-row">
+            <a href="https://www.instagram.com/p/BBzTlXFkhYCSH_DcDKke3MEr1sE8kk9roR4nKk0/" target="_blank">
+              <img src="img/img1.jpg" alt="">
+            </a>
+            <a href="https://www.instagram.com/p/BkvjrxOgA1tkGmBe7hROouJKrXVm34vAoYnXtU0/" target="_blank">
+              <img src="img/img2.jpg" alt="">
+            </a>
+            <a href="https://www.instagram.com/p/BcII-BtlxDhAeEDj9art4HMAnHglvPFSt_jdhU0/" target="_blank">
+              <img src="img/img3.jpg" alt="">
+            </a>
+            <a href="https://www.instagram.com/p/BW-6P-GF6KE24zonrLmahAjskaYMvTmFJS186Q0/" target="_blank">
+              <img src="img/img4.jpg" alt="">
+            </a>
+          </div>
+          <div class="insta-row">
+            <a href="https://www.instagram.com/p/BgWoFDLlJyEqbcS678HtGJtpVm6TyOQydQ74iM0/" target="_blank">
+              <img src="img/img5.jpg" alt="">
+            </a>
+            <a href="https://www.instagram.com/p/BT2AW9_gLhqbHmvB0DHR62VLVLbXkrQQbQSD4o0/" target="_blank">
+              <img src="img/img6.jpg" alt="">
+            </a>
+            <a href="https://www.instagram.com/p/BmBQXcYlRj7plwdzi8gia69dbGiwVbnAE7r5pU0/" target="_blank">
+              <img src="img/img7.jpg" alt="">
+            </a>
+            <a class="img8" href="https://www.instagram.com/p/Bqnv4YGhqaZ4NLFDu5UwvqQnw6RZLryHjupjNM0/" target="_blank">
+              <img src="img/img8.jpg" alt="">
+            </a>
+          </div>
+        </div>
+      </div>
+      <div id="contact">
+        <p class="text">Contact Us</p>
+        <div class="contact-box">
+    			<form method="post">
+            <a>
+              <label>Nom</label>
+              <input type="text" name="nom" required />
+            </a>
+            <a>
+              <label>Prénom</label>
+              <input type="text" name="prenom" required />
+            </a>
+            <a>
+              <label>Email</label>
+              <input type="email" name="email" required />
+            </a>
+            <a>
+              <input class="button" type="submit" />
+            </a>
+      		</form>
+          <?php
+          getenv('SENDGRID_API_KEY');
+          if(isset($_POST['nom'], $_POST['prenom'], $_POST['email'])){
+            $position_arobase=strpos($_POST['email'],'@');
+            if($position_arobase===false){
+              echo '<p>Votre email doit comporter un arobase.</p>';
+            }
+            else {
+              require 'vendor/autoload.php';
+              $email = new \SendGrid\Mail\Mail();
+              $email->setFrom($_POST['email'], $_POST['nom'].' '.$_POST['prenom']);
+              $email->setSubject("Sending by augustin.ribreau.co");
+              $email->addTo("augustin.ribreau@gmail.com", "Example User");
+              $email->addContent("text/plain", "and easy to do anywhere, even with PHP");
+              $email->addContent(
+                'text/html', 'Voici ces informations : '.$_POST['email'].','. $_POST['nom'].','.$_POST['prenom']
+              );
+              $sendgrid = new \SendGrid(getenv('SENDGRID_API_KEY'));
+              try {
+                $response = $sendgrid->send($email);
+              } catch (Exception $e) {
+                echo 'Caught exception: '. $e->getMessage() ."\n";
+              }
+            }
+          }
+          ?>
+        </div>
+      </div>
+    </div>
+    <footer>
+      <p>© Augustin Ribreau - All Rights Reserved.</p>
+    </footer>
+  </body>
+</html>
