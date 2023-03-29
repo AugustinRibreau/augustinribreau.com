dfefb799cd1a6750175942e8c695331cbf4e2830
Augustin <augustin.ribreau@gmail.com>
2023-03-29 19:16:01 +0200

    fix: SEO

--- nuxt.config.js
+++ nuxt.config.js
@@ -12,12 +12,36 @@ export default {
       lang: 'fr'
     },
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
+      {charset: 'utf-8'},
+      {name: 'viewport', content: 'width=device-width, initial-scale=1'},
+      {
+        hid: 'description',
+        name: 'description',
+        content: 'Augustin Ribreau. Passionate full-stack developer in 5th year of engineering school dedicated to expanding knowledge and skills in tech. Ambitious to work on innovative projects and make valuable contributions to a stimulating environment.'
+      },
+      {name: 'format-detection', content: 'telephone=no'},
+      {name: "author", content: "Augustin Ribreau"},
+      {
+        name: "keywords",
+        content: "Augustin, Ribreau, Augustin Ribreau, AugustinRibreau, augustinRibreau, Augustinribreau, augustinribreau, augustin, ribreau, developpeur, full stack, fullstack, developpeur fullstack, Troupe Les Herbiers, Scoutmaster"
+      },
+      {property: "og:title", content: "Augustin Ribreau"},
+      {property: "og:type", content: "website"},
+      {property: "og:url", content: "https://augustinribreau.com/"},
+      {property: "og:site_name", content: "Augustin Ribreau"},
+      {
+        property: "og:description",
+        content: "Augustin Ribreau. Passionate full-stack developer in 5th year of engineering school dedicated to expanding knowledge and skills in tech. Ambitious to work on innovative projects and make valuable contributions to a stimulating environment."
+      },
+      {property: "og:locale", content: "fr_FR"},
+      {property: "twitter:url", content: "https://augustinribreau.com/"},
+      {property: "twitter:title", content: "Augustin Ribreau"},
+      {
+        property: "twitter:description",
+        content: "Augustin Ribreau. Passionate full-stack developer in 5th year of engineering school dedicated to expanding knowledge and skills in tech. Ambitious to work on innovative projects and make valuable contributions to a stimulating environment."
+      }
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    link: [{rel: 'icon', type: 'image/x-icon', href: '/favicon.ico'}]
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
